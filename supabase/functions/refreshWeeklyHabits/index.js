936b766720fa49af023d822d6f436efe10f10926
zlatanoski <zlatanoskidavid92@gmail.com>
2025-08-07 23:35:43 +0200

    Edge function coded but missing automation to run as scheduled process

--- supabase/functions/refreshWeeklyHabits/index.js
+++ supabase/functions/refreshWeeklyHabits/index.js
@@ -5,4 +5,42 @@ const supabase = createClient (
     Deno.env.get("SUPABASE_URL"),
     Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")
 
-)
+);
+
+function IsMondayMidnight(timezone){
+
+    const now = Temporal.Now.zonedDateTimeISO(timezone);
+
+    if(now.dayOfWeek === 1 && now.hour === 0){
+        return true
+    } else {
+        return false
+    }
+
+}
+
+Deno.serve(async()=>{
+
+    const {data:users,errors} = await supabase.from('profiles').select("id, timezone");
+
+    if(errors){
+        console.log(errors)
+        return new Response("Error",{status:500});
+    }
+
+    for(const user of users){
+        const timeZone = user.timezone;
+
+        if(IsMondayMidnight(timeZone)){
+            console.log(`Would delete habits for user: ${user.id}`);
+        }
+
+
+    }
+
+
+    return new Response("Checked all users", { status: 200 });
+
+
+
+})
